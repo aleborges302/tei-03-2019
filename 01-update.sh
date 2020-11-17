bc6b412357950cb884d603f0e39d63de980890e3
aleborges302 <74630325+aleborges302@users.noreply.github.com>
2020-11-17 20:19:17 -0300

    Add files via upload

---
+++ 01-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "Atualizando as listas do Apt"
+    apt update
+
+echo -e "Atualizando o software"
+    apt -y upgrade
+    
+echo -e "Forçando a atualização do sistema"
+    apt -y full-apdate
+
+echo -e "Forçando a atualização de segurança"
+    apt -y dist-upgrade
+
+echo -e "Removendo software desnecessario"
+   apt -y autoremove
+   apt -y autoremove
+   apt clean
